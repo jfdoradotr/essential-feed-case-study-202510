0d87d0c2e4d28d314ff460e1f724e1f39fd21e9d
Juan Dorado <jfdoradotr@icloud.com>
2025-10-29 16:25:49 -0600

    Move `RemoteFeedLoader` components to production

---
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,23 @@
+//
+//  Copyright © Juan Francisco Dorado Torres. All rights reserved.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+  func get(from url: URL)
+}
+
+public final class RemoteFeedLoader {
+  private let url: URL
+  private let client: HTTPClient
+
+  public init(url: URL = URL(string: "https://a-url.com")!, client: HTTPClient) {
+    self.url = url
+    self.client = client
+  }
+
+  public func load() {
+    client.get(from: url)
+  }
+}
