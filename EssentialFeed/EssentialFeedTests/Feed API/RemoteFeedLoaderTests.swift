9d85a3fce862e8d34c2b3827ba9f14c8502420c0
Juan Dorado <jfdoradotr@icloud.com>
2025-10-30 11:20:19 -0600

    Remove the generic error type from the `LoadFeedResult` to simplify type constraints

--- EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -34,7 +34,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
   func test_load_deliversErrorOnClientError() {
     let (sut, client) = makeSUT()
 
-    expect(sut, toCompleteWith: .failure(.connectivity), when: {
+    expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.connectivity), when: {
       let clientError = NSError(domain: "Test", code: 0)
       client.complete(with: clientError)
     })
@@ -45,7 +45,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
 
     let samples = [199, 201, 300, 400, 500]
     samples.enumerated().forEach { index, code in
-      expect(sut, toCompleteWith: .failure(.invalidData), when: {
+      expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData), when: {
         let json = makeItemsJSON([])
         client.complete(withStatusCode: code, data: json, at: index)
       })
@@ -55,7 +55,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
   func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
     let (sut, client) = makeSUT()
 
-    expect(sut, toCompleteWith: .failure(.invalidData), when: {
+    expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData), when: {
       let invalidJSON = Data("invalid json".utf8)
       client.complete(withStatusCode: 200, data: invalidJSON)
     })
@@ -171,7 +171,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
       case let (.success(receivedItems), .success(expectedItems)):
         XCTAssertEqual(receivedItems, expectedItems, file: file, line: line)
 
-      case let (.failure(receivedError), .failure(expectedError)):
+      case let (.failure(receivedError as RemoteFeedLoader.Error), .failure(expectedError as RemoteFeedLoader.Error)):
         XCTAssertEqual(receivedError, expectedError, file: file, line: line)
 
       default:
