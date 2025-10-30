1b250fc96cba55ee6ec8f8dc615803ff12d49e8c
Juan Dorado <jfdoradotr@icloud.com>
2025-10-29 18:46:02 -0600

    Move `HTTPClient` to its own file

---
+++ EssentialFeed/EssentialFeed/Feed API/HTTPClient.swift
@@ -0,0 +1,14 @@
+//
+//  Copyright © Juan Francisco Dorado Torres. All rights reserved.
+//
+
+import Foundation
+
+public enum HTTPClientResult {
+  case success(Data, HTTPURLResponse)
+  case failure(Error)
+}
+
+public protocol HTTPClient {
+  func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
